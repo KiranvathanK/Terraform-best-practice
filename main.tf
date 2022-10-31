2e09f989b0490e52904ee2d513e6a6dbf5e108a1
kiranvathank <kirankvathan@gmail.com>
2022-10-31 09:47:45 +0000

    Use the self variable

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
-resource "aws_instance" "ec2"{
+resource "aws_instance" "e1"{
   ami           = var.instance_1_ami
   instance_type = var.instance_1_type
   tags = {
     Name = var.instance_1_name
+  }
+}
+
+resource "aws_instance" "e2" {
+  ami           = var.instance_2_ami
+  instance_type = var.instance_2_type
+  tags = {
+    Name = var.instance_2_name
+  }
+  provisioner "local-exec" {
+    command    = "echo The IP address of the Server is ${self.private_ip}"
+    on_failure = continue
   }
 }
--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
 output "instance_1_id" {
   description = "The ID of the instance-1"
-  value       = try(aws_instance.ec2.id)
+  value       = try(aws_instance.e1.id)
+}
+
+output "instance_2_id" {
+  description = "The ID of the instance-2"
+  value       = try(aws_instance.e2.id)
 }
